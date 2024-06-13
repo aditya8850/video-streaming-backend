9b2292b1759f7790439cc70d4a2ffc0ccbd107c3
aditya8850 <kalsotraaditya98@gmail.com>
2024-06-14 01:02:06 +0530

    utils-add

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+// setting up express app
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+); //setting cors
+app.use(express.json({ limit: "16kb" })); //setting up express to accept json
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //this allows the use of adding extended objects
+app.use(express.static("public"));
+app.use(cookieParser())
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config(); // Load environment variables
-
-
 import connectDB from "./db/index.js"; // Ensure this path is correct
-connectDB();
+import  {app}  from "./app.js";
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT , () => {
+      console.log(`Server listening at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed:", err);
+  });
