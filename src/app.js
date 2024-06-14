846d078b8a00d7cb57a3b9a82741fdcc03fac0c5
aditya8850 <kalsotraaditya98@gmail.com>
2024-06-15 01:35:25 +0530

    all-setup-configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.json({ limit: "16kb" })); //setting up express to accept json
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //this allows the use of adding extended objects
 app.use(express.static("public"));
 app.use(cookieParser())
+
+//routes import
+import router from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", router); //users routes
 export { app };
